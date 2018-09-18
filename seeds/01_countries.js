bfdbebcae6dd05d59be3f1679194038bb0e559c4
Danny Pifer <piferdg@gmail.com>
2018-09-17 20:41:49 -0600

    Adding seeds. Languages seed finished, countries seed almost finished

--- seeds/01_countries.js
+++ seeds/01_countries.js
@@ -1,13 +1,136 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('countries').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('countries').insert([
+        {
+          countryName: 'Spain',
+          currency: 'Euro',
+          attractionOne: 'The Alhambra',
+          attractionTwo: 'Sagrada Familia',
+          attractionThree: 'Mezquita of Cordoba'
+        },
+        {
+          countryName: 'France',
+          currency: 'Euro',
+          attractionOne: 'Eiffel Tower',
+          attractionTwo: 'Louvre',
+          attractionThree: 'Notre-Dame de Paris'
+        },
+        {
+          countryName: 'Russia',
+          currency: 'Ruble',
+          attractionOne: 'Moscow Kremlin',
+          attractionTwo: 'Red Square',
+          attractionThree: 'Hermitage Museum'
+        },
+        {
+          countryName: 'China',
+          currency: 'Renminbi',
+          attractionOne: 'Forbidden City',
+          attractionTwo: 'Great Wall of China',
+          attractionThree: 'Terracotta Army'
+        },
+        {
+          countryName: 'Egypt',
+          currency: 'Egyptian pound',
+          attractionOne: 'Pyramids of Giza',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Turkey',
+          currency: 'Lira',
+          attractionOne: 'Sultan Ahmed Mosque',
+          attractionTwo: 'Hagia Sophia',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Greece',
+          currency: 'Euro',
+          attractionOne: 'Acropolis of Athens',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Japan',
+          currency: 'Yen',
+          attractionOne: 'Mount Fuji',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Morocco',
+          currency: 'Moroccan dirham',
+          attractionOne: 'Casablanca',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Portugal',
+          currency: 'Euro',
+          attractionOne: 'Porto',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Mexico',
+          currency: 'Peso',
+          attractionOne: 'Tulum',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Belize',
+          currency: 'Belize dollar',
+          attractionOne: '',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Brazil',
+          currency: 'Real',
+          attractionOne: 'Ambergris Caye',
+          attractionTwo: 'Caracol',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Netherlands',
+          currency: 'Euro',
+          attractionOne: 'Amsterdam',
+          attractionTwo: 'Van Gogh House',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Vietnam',
+          currency: 'Dong',
+          attractionOne: 'Halong Bay',
+          attractionTwo: '',
+          attractionThree: ''
+        },
+        {
+          countryName: 'India',
+          currency: 'Indian rupee',
+          attractionOne: 'Taj Mahal',
+          attractionTwo: 'Mahabodhi Temple',
+          attractionThree: 'Mysore Palace'
+        },
+        {
+          countryName: 'Germany',
+          currency: 'Euro',
+          attractionOne: 'Cologne',
+          attractionTwo: 'Dresden',
+          attractionThree: ''
+        },
+        {
+          countryName: 'Italy',
+          currency: 'Euro',
+          attractionOne: 'The Colosseum',
+          attractionTwo: 'Florence Cathedral',
+          attractionThree: ''
+        },
       ]);
     });
 };
--- seeds/02_languages.js
+++ seeds/02_languages.js
@@ -1,13 +1,266 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('languages').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('languages').insert([
+        {
+          languageName: 'Afrikaans',
+          languageCode: 'af'
+        },
+        {
+          languageName: 'Albanian',
+          languageCode: 'sq'
+        },
+        {
+          languageName: 'Arabic',
+          languageCode: 'ar'
+        },
+        {
+          languageName: 'Azerbaijani',
+          languageCode: 'az'
+        },
+        {
+          languageName: 'Basque',
+          languageCode: 'eu'
+        },
+        {
+          languageName: 'Bengali',
+          languageCode: 'bn'
+        },
+        {
+          languageName: 'Belarusian',
+          languageCode: 'be'
+        },
+        {
+          languageName: 'Bulgarian',
+          languageCode: 'bg'
+        },
+        {
+          languageName: 'Catalan',
+          languageCode: 'ca'
+        },
+        {
+          languageName: 'Chinese Simplified',
+          languageCode: 'zh-CN'
+        },
+        {
+          languageName: 'Chinese Traditional',
+          languageCode: 'zh-TW'
+        },
+        {
+          languageName: 'Croatian',
+          languageCode: 'hr'
+        },
+        {
+          languageName: 'Czech',
+          languageCode: 'cs'
+        },
+        {
+          languageName: 'Danish',
+          languageCode: 'da'
+        },
+        {
+          languageName: 'Dutch',
+          languageCode: 'nl'
+        },
+        {
+          languageName: 'English',
+          languageCode: 'en'
+        },
+        {
+          languageName: 'Esperanto',
+          languageCode: 'eo'
+        },
+        {
+          languageName: 'Estonian',
+          languageCode: 'et'
+        },
+        {
+          languageName: 'Filipino',
+          languageCode: 'tl'
+        },
+        {
+          languageName: 'Finnish',
+          languageCode: 'fi'
+        },
+        {
+          languageName: 'French',
+          languageCode: 'fr'
+        },
+        {
+          languageName: 'Galician',
+          languageCode: 'gl'
+        },
+        {
+          languageName: 'Georgian',
+          languageCode: 'ka'
+        },
+        {
+          languageName: 'German',
+          languageCode: 'de'
+        },
+        {
+          languageName: 'Greek',
+          languageCode: 'el'
+        },
+        {
+          languageName: 'Gujarati',
+          languageCode: 'gu'
+        },
+        {
+          languageName: 'Haitian Creole',
+          languageCode: 'ht'
+        },
+        {
+          languageName: 'Hebrew',
+          languageCode: 'iw'
+        },
+        {
+          languageName: 'Hindi',
+          languageCode: 'hi'
+        },
+        {
+          languageName: 'Hungarian',
+          languageCode: 'hu'
+        },
+        {
+          languageName: 'Icelandic',
+          languageCode: 'is'
+        },
+        {
+          languageName: 'Indonesian',
+          languageCode: 'id'
+        },
+        {
+          languageName: 'Irish',
+          languageCode: 'ga'
+        },
+        {
+          languageName: 'Italian',
+          languageCode: 'it'
+        },
+        {
+          languageName: 'Japanese',
+          languageCode: 'ja'
+        },
+        {
+          languageName: 'Kannada',
+          languageCode: 'kn'
+        },
+        {
+          languageName: 'Korean',
+          languageCode: 'ko'
+        },
+        {
+          languageName: 'Latin',
+          languageCode: 'la'
+        },
+        {
+          languageName: 'Latvian',
+          languageCode: 'lv'
+        },
+        {
+          languageName: 'Lithuanian',
+          languageCode: 'lt'
+        },
+        {
+          languageName: 'Macedonian',
+          languageCode: 'mk'
+        },
+        {
+          languageName: 'Malay',
+          languageCode: 'ms'
+        },
+        {
+          languageName: 'Maltese',
+          languageCode: 'mt'
+        },
+        {
+          languageName: 'Norwegian',
+          languageCode: 'no'
+        },
+        {
+          languageName: 'Persian',
+          languageCode: 'fa'
+        },
+        {
+          languageName: 'Polish',
+          languageCode: 'pl'
+        },
+        {
+          languageName: 'Portuguese',
+          languageCode: 'pt'
+        },
+        {
+          languageName: 'Romanian',
+          languageCode: 'ro'
+        },
+        {
+          languageName: 'Russian',
+          languageCode: 'ru'
+        },
+        {
+          languageName: 'Serbian',
+          languageCode: 'sr'
+        },
+        {
+          languageName: 'Slovak',
+          languageCode: 'sk'
+        },
+        {
+          languageName: 'Slovenian',
+          languageCode: 'sl'
+        },
+        {
+          languageName: 'Spanish',
+          languageCode: 'es'
+        },
+        {
+          languageName: 'Swahili',
+          languageCode: 'sw'
+        },
+        {
+          languageName: 'Swedish',
+          languageCode: 'sv'
+        },
+        {
+          languageName: 'Tamil',
+          languageCode: 'ta'
+        },
+        {
+          languageName: 'Telugu',
+          languageCode: 'te'
+        },
+        {
+          languageName: 'Thai',
+          languageCode: 'th'
+        },
+        {
+          languageName: 'Turkish',
+          languageCode: 'tr'
+        },
+        {
+          languageName: 'Ukrainian',
+          languageCode: 'uk'
+        },
+        {
+          languageName: 'Urdu',
+          languageCode: 'ur'
+        },
+        {
+          languageName: 'Vietnamese',
+          languageCode: 'vi'
+        },
+        {
+          languageName: 'Welsh',
+          languageCode: 'cy'
+        },
+        {
+          languageName: 'Yiddish',
+          languageCode: 'yi'
+        }
       ]);
     });
 };
